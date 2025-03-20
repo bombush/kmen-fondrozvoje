90f46378539d6f1e61b2181976e34154f18eed7d
Stribro @ XPS Debian <quidible@gmail.com>
2025-03-20 10:17:11 +0100

    added global logging of errors on queries

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,22 @@
 "use client"
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { QueryCache, MutationCache, QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useState } from "react"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(() => new QueryClient({
+    queryCache: new QueryCache({
+      onError: (error: Error) => {
+        console.error(`Query error:`, error)
+      }
+    }),
+    mutationCache: new MutationCache({
+      onError: (error: Error) => {
+        console.error(`Mutation error:`, error)
+      }
+    })
+  }))
+
 
   return (
     <QueryClientProvider client={queryClient}>
