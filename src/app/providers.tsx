7a1657303f9b231edaa841322c5ed4efad0360bd
Stribro @ XPS Debian <quidible@gmail.com>
2025-04-23 11:42:01 +0200

    added tanstack dev tools

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,7 @@
 
 import { QueryCache, MutationCache, QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useState } from "react"
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({
     queryCache: new QueryCache({
@@ -21,6 +21,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 } 
